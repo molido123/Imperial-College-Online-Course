f769fe6a2af080f2f0c4dc8407b833ab8c86cf31
huagenda <3391394571@qq.com>
2023-02-28 00:47:56 +0800

    we can get a adj frequency statics now

--- FinalWork/initializer.py
+++ FinalWork/initializer.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 
+import nltk
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -9,16 +10,51 @@ from nltk.corpus import stopwords
 noise = stopwords.words("english")
 
 
-# detect meaningless words
-def is_meaningless_str(s):
-    # meaningfully word won't be too long
-    if len(s) > 10 or not set(s).issubset(set('0123456789abcdef')):
-        return True
-    # after removing duplicated words, if the length reduce too much, we can assume it may be meaningless.
-    return len(set(s)) < len(s)
+def part_of_speech(words):
+    # Define the classes and their corresponding keywords
+    data = []
+    # set the sample size
+    count = 1000000
+    for sent in words:
+        if count < 0:
+            break
+        count -= 1
+        data.append(nltk.pos_tag(nltk.word_tokenize(sent)))
+    # collect all the words with label JJ
+    adj = []
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j][1] == "JJ":
+                adj.append(data[i][j][0])
+    words_count = Counter(adj)
+    # depict the picture of most common adj
+    top_words = words_count.most_common(10)  # Change the number to show more/less words
+    x_values = [word[0] for word in top_words]
+    y_values = [word[1] for word in top_words]
+    plt.bar(x_values, y_values)
+    plt.xlabel('Word')
+    plt.xticks(rotation=45)
+    plt.ylabel('Frequency')
+    plt.title('Most Commonly Used adj Words')
+    plt.savefig("Most Commonly Used adj Words.png")
+    plt.show()
 
 
-# only can handle one word at a time
+def general_counter(words):
+    # Count the frequency of each word
+    # this Counter function is from Collections library
+    word_counts = Counter(words)
+    # Create a bar chart of the most common words
+    top_words = word_counts.most_common(10)  # Change the number to show more/less words
+    x_values = [word[0] for word in top_words]
+    y_values = [word[1] for word in top_words]
+    plt.bar(x_values, y_values)
+    plt.xlabel('Word')
+    plt.ylabel('Frequency')
+    plt.xticks(rotation=45)
+    plt.title('Most Commonly Used Words in general')
+    plt.savefig("Most Commonly Used Words in general.png")
+    plt.show()
 
 
 def main():
@@ -40,18 +76,9 @@ def main():
     # Remove stop words
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if not word in stop_words]
-    # Count the frequency of each word
-    # this Conter function is from Collections library
-    word_counts = Counter(words)
-    # Create a bar chart of the most common words
-    top_words = word_counts.most_common(10)  # Change the number to show more/less words
-    x_values = [word[0] for word in top_words]
-    y_values = [word[1] for word in top_words]
-    plt.bar(x_values, y_values)
-    plt.xlabel('Word')
-    plt.ylabel('Frequency')
-    plt.title('Most Commonly Used Words in general')
-    plt.savefig("Most Commonly Used Words in general.png")
+    #
+    general_counter(words)
+    part_of_speech(words)
 
 
 if __name__ == '__main__':
